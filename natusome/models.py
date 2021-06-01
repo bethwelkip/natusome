13cc9f1fa4d240da7482d2325f73a384b3834f01
Bethwel Kiplimo <bkiplimo@princeton.edu>
2021-06-01 08:19:40 -0400

    add capability to receive and process message

--- natusome/models.py
+++ natusome/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+
+
+class Content(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    content = models.TextField()
+    value = models.FloatField(default=100)
+
+    def get_content(self, x=5):
+        content = self.objects.order_by('value')[:x]
+        return content
+        
--- natusome/views.py
+++ natusome/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
-
+from twilio.twiml.messaging_response import MessagingResponse
+from .models import Customer, Content
 # Create your views here.
+
+
+def index():
+    return ""
+
+
+# at this time i am just coding the methods I will need.  I will integrate it in a bit
+def send_message():
+    response = MessagingResponse()
+    contents = Content.get_content()
+    texts = ""
+    for content in contents:
+        value = content.value*(10**(-1/content.value))
+        update = Content.filter(id=content.id).update(value=value)
+        update.save()
+        texts += str(content.content) + "\n"
+    response.message(texts)
+
+    return str(response)
+
+
+def receive_message(request):
+    msg = request.form.get('Body').lower()
+    user = request.form.get('From').lower()
+    response = MessagingResponse()
+    text = Content.get_content()
+    resp = ""
+    current_subject = ""
+    if msg == "join egg-unusual" or msg == "hi":
+        return send_message(user)
+    else:
+        user = Customer.objects.filter(phone=user).first()
+        if user:
+            new_content = Content(name=user, user_id=user.id, content=msg)
+            new_content.save()
+        else:
+            new_user = Customer(name=user, phone=user)
+            new_user.save()
+            new_content = Content(name=new_user.name,
+                                  user_id=new_user.id, content=msg)
+            new_content.save()
+        return send_message(user)
+
+    return str(response)
