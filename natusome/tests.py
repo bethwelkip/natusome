aac04a3225e47e01b34fc7ac9fd53cdd7bda31f7
Bethwel Kiplimo <bkiplimo@princeton.edu>
2021-08-23 10:49:14 -0400

    boiler plate for tests

--- natusome/tests.py
+++ natusome/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
+from .models import Customer, Content
 
-# Create your tests here.
+# Tests appear below this.
+'''
+Models tests
+'''
+class CustomerModel(TestCase):
+
+    def setUp(self):
+        self.customer = Customer(name="whatsapp:+12345679842", phone = "whatsapp:+12345679842")
+    
+    def test_instance(self):
+        self.customer.save()
+        self.assertTrue(isinstance(self.customer, Customer))
+
+    def test_get_content(self):
+        cont = self.customer.get_content(5)
+        self.assertTrue(cont==sorted(cont)[::-1])) # confirm that content exists and sorted in descending order
+
+
+    def tearDown(self):
+        Customer.objects.all().delete()
+        self.assertTrue(len(Customer.objects.all())==0)
+
+class ContentModel(TestCase):
+    pass
+
+
+
+'''
+Views Tests
+'''
+class CustomerModel(TestCase):
+    pass
+
+
+
+'''
+Urls tests
+'''
+class CustomerModel(TestCase):
+    pass
